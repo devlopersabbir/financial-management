ed8892b72e4af50d5dd435cc966f9f22dcd9e47c
devlopersabbir <devlopersabbir@gmail.com>
2025-05-19 04:07:41 +0600

    user controoler and packagejjson 🥖

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import * as schema from '../database/schema';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    private readonly userService: UsersService,
+    @Inject()
+    private readonly db: NodePgDatabase<typeof schema>,
+  ) {}
+
+  @Post()
+  async create(@Body() createDto: any) {
+    console.log(createDto);
+    return 'created!';
+  }
+
+  @Get('/fetch')
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}
